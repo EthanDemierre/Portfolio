7c7a17f3f497c4796bbde5f85729662f4ec86968
EthanDemierre <ethandemierre20@gmail.com>
2026-05-29 06:14:27 +0200

    add: favicon + img about

--- .nuxt/dist/server/client.manifest.mjs
+++ .nuxt/dist/server/client.manifest.mjs
@@ -1,1 +1,1 @@
-export { default } from "file:///root/app/code/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
+export { default } from "file:///Users/ethandemierre/Desktop/github/Portfolio-demierree-ethan-2026/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
